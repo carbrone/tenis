7ada1558998d2296808e6702eb6e24306165668d
carbrone <carson.li705@gmail.com>
2026-08-08 00:32:50 -0400

    Update buton.py

--- buton.py
+++ buton.py
@@ -2,13 +2,23 @@ import keyboard
 import pyautogui as py
 import time
 import mouse
-import cv2
+import os
+
+script = os.path.dirname(os.path.abspath(__file__))
+image = os.path.join(script, "reservebutton.png")
 
 time.sleep(1)
 mouse.wait("left")
 print("non")
 x, y = py.position()
-time.sleep(1)
-loc = py.locateOnScreen(r"C:\Users\carso\OneDrive\Documents\GitHub\tenis\reservebutton.png", confidence=0.8)
+loc = None
+i=0
+while loc is None:
+    try:
+        loc = py.locateOnScreen(image, confidence=0.8)
+    except:
+        time.sleep(0.1)
+        i+=1
 print (loc) 
+print(i)
 py.moveTo(loc)
